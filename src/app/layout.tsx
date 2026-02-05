a50391c835ac7947b9c325374cb3f02a9e17b100
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-05 10:46:47 -0800

    chore: setup base layout with header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin'],
@@ -24,7 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
   );
 }
