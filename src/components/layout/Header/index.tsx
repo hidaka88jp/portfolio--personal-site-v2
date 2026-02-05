ab989e52a8645cd4071ec8a985a1b907c5913eb3
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-05 12:54:34 -0800

    feat: Add basic header structure

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,7 +1,25 @@
+import { Antonio } from 'next/font/google';
+import Link from 'next/link';
+
+const antonio = Antonio({
+  subsets: ['latin'],
+});
+
 export default function Header() {
   return (
-    <header>
-      <p className='text-center'>Header</p>
+    <header className='px-4 pt-4 pb-5 sm:px-8 sm:pt-9 sm:pb-7'>
+      <div className='mx-auto flex w-full max-w-94 items-center justify-between sm:max-w-5xl sm:items-baseline'>
+        <Link href='/'>
+          <h1
+            className={`${antonio.className} text-3xl tracking-[.18em] sm:text-4xl`}
+            aria-label='Takanori Hidaka'
+          >
+            <span className='text-accent'>T</span>AKANORI <span className='text-accent'>H</span>
+            IDIKA
+          </h1>
+        </Link>
+        <div>Nav</div>
+      </div>
     </header>
   );
 }
