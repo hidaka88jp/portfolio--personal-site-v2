d39a948b8e4423375fd1883cea3fe49d28a6510e
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-05 13:54:40 -0800

    feat: add navigation constants

---
+++ src/constants/navigation.ts
@@ -0,0 +1,10 @@
+export const NAV_LINKS = [
+  { href: '/#about', label: 'About' },
+  { href: '/#works', label: 'Works' },
+  { href: '/notes', label: 'Notes' },
+] as const;
+
+export const SOCIAL_LINKS = [
+  { href: 'https://ca.linkedin.com/in/takanori-hidaka-102568177', label: 'LinkedIn' },
+  { href: 'https://x.com/taka_hidaka_log', label: 'X / Twitter' },
+] as const;
