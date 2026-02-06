e60f009ef817311a98c1f29669dac23571d779f9
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-06 14:12:17 -0800

    feat: create skeleton layout for About section

--- src/components/top/About/index.tsx
+++ src/components/top/About/index.tsx
@@ -1,10 +1,43 @@
+import Image from 'next/image';
+
 import TopSectionTitle from '@/components/shared/TopSectionTitle';
 
 export default function About() {
   return (
-    <section id='about' className='px-4 pb-16 sm:px-8 sm:pb-20'>
+    <section id='about' className='px-4 pb-16 sm:px-8 sm:pb-28'>
       <div className='mx-auto w-full max-w-94 sm:max-w-5xl'>
         <TopSectionTitle title='About' subTitle='Introduce Myself' />
+        <div className='grid gap-7 lg:grid-cols-2 lg:gap-10'>
+          <Image
+            src='/images/about.webp'
+            alt='About Image'
+            width={3024}
+            height={3780}
+            className='aspect-square w-full object-cover object-bottom sm:aspect-3/2 sm:object-center lg:order-2 lg:aspect-auto'
+          />
+          <div className='w-full lg:order-1'>
+            <div className='mb-4 flex flex-col items-center lg:items-start'>
+              <h3 className='font-inconsolata mb-1 text-xl font-medium sm:font-normal'>Who I am</h3>
+              <div className='bg-accent h-0.5 w-8' />
+            </div>
+            <p className='mb-7 sm:mb-9'>
+              Hi, I&apos;m Taka. I’m a self-taught web developer, currently focusing on frontend.
+              <br />
+              <br />
+              After spending time reflecting on the right path for me, I’ve chosen to pursue web
+              development seriously. I’ve built several portfolio projects and experimented with
+              various technologies.
+              <br />
+              <br />
+              I’m committed to growing in this field long-term and am always learning new skills.
+            </p>
+            <div className='mb-4 flex flex-col items-center lg:items-start'>
+              <h3 className='font-inconsolata mb-1 text-xl font-medium sm:font-normal'>Skills</h3>
+              <div className='bg-accent h-0.5 w-8' />
+            </div>
+            {/* Skills list with tech stack badge */}
+          </div>
+        </div>
       </div>
     </section>
   );
