5eda288f41661ebf76f4c57403abe50f33d2aa2f
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-09 12:35:36 -0800

    chore: remove unused getTechStack helper

--- src/lib/getTechStack.ts
+++ src/lib/getTechStack.ts
@@ -1,10 +1,6 @@
 import { TECH_STACKS } from '@/constants/techStacks';
 import type { TechStack } from '@/constants/techStacks';
 
-export function getTechStack(id: string): TechStack | undefined {
-  return TECH_STACKS.find((stack) => stack.id === id);
-}
-
 export function getTechStacksByIds(ids: string[]): TechStack[] {
   const techStackMap = new Map(TECH_STACKS.map((stack) => [stack.id, stack]));
 
