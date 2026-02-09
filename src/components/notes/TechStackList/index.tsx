b3780f2c680d20049e3c7e5aedb674118c4a4cc1
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-09 12:21:07 -0800

    refactor: introduce getTechStacksByIds for TechStackList

--- src/components/notes/TechStackList/index.tsx
+++ src/components/notes/TechStackList/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import type { TechStack } from '@/lib/microcms';
-import { getTechStack } from '@/lib/getTechStack';
+import { getTechStacksByIds } from '@/lib/getTechStack';
 import TechStackBadge from '@/components/shared/TechStackBadge';
 
 type Props = {
@@ -8,23 +8,18 @@ type Props = {
 };
 
 export default function TechStackList({ techStacks }: Props) {
+  const stacks = getTechStacksByIds(techStacks.map((s) => s.id));
+
   return (
     <ul className='flex flex-wrap gap-3 lg:flex-col'>
       <li className='border-gray flex w-fit items-center rounded-md border px-2 py-1'>
         <Link href='/notes'>ALL</Link>
       </li>
-      {techStacks.map((techStack) => {
-        const localStack = getTechStack(techStack.id);
-        if (!localStack) return null;
-
+      {stacks.map((stack) => {
         return (
-          <li key={localStack.id} className='w-fit'>
-            <Link href={`/notes?category=${localStack.id}`}>
-              <TechStackBadge
-                name={localStack.name}
-                Icon={localStack.Icon}
-                color={localStack.color}
-              />
+          <li key={stack.id} className='w-fit'>
+            <Link href={`/notes?category=${stack.id}`}>
+              <TechStackBadge name={stack.name} Icon={stack.Icon} color={stack.color} />
             </Link>
           </li>
         );
