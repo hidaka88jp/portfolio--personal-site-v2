1c9d3eaec157fdf6524357490617f6d441b0ff76
hidaka88jp <hidaka88jp@yahoo.co.jp>
2026-02-10 11:31:28 -0800

    chore: temporarily disable sitemap generation

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,4 +1,4 @@
-// import type { MetadataRoute } from 'next';
+import type { MetadataRoute } from 'next';
 // import { getAllWorksList, getAllNotesList } from '@/lib/microcms';
 
 // export const revalidate = 60 * 60 * 24; // 1 day
@@ -12,31 +12,32 @@
 
 // const buildUrl = (path = '') => `${siteUrl}${path}`;
 
-// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-//   const worksContent = await getAllWorksList();
-//   const notesContent = await getAllNotesList();
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [];
+  // const worksContent = await getAllWorksList();
+  // const notesContent = await getAllNotesList();
 
-//   const worksUrls: MetadataRoute.Sitemap = worksContent.map((content) => ({
-//     url: buildUrl(`/${content.id}`),
-//     lastModified: content.updatedAt,
-//   }));
-//   const notesUrls: MetadataRoute.Sitemap = notesContent.map((content) => ({
-//     url: buildUrl(`/notes/${content.id}`),
-//     lastModified: content.updatedAt,
-//   }));
+  // const worksUrls: MetadataRoute.Sitemap = worksContent.map((content) => ({
+  //   url: buildUrl(`/${content.id}`),
+  //   lastModified: content.updatedAt,
+  // }));
+  // const notesUrls: MetadataRoute.Sitemap = notesContent.map((content) => ({
+  //   url: buildUrl(`/notes/${content.id}`),
+  //   lastModified: content.updatedAt,
+  // }));
 
-//   const now = new Date();
+  // const now = new Date();
 
-//   return [
-//     {
-//       url: buildUrl(''),
-//       lastModified: now,
-//     },
-//     {
-//       url: buildUrl('/notes'),
-//       lastModified: now,
-//     },
-//     ...worksUrls,
-//     ...notesUrls,
-//   ];
-// }
+  // return [
+  //   {
+  //     url: buildUrl(''),
+  //     lastModified: now,
+  //   },
+  //   {
+  //     url: buildUrl('/notes'),
+  //     lastModified: now,
+  //   },
+  //   ...worksUrls,
+  //   ...notesUrls,
+  // ];
+}
